b70c06c7a6558c5b037cd2b3195dda7219f2ccef
valdetad <valdeta.demiri@hotmail.com>
2024-07-19 09:41:04 +0100

    ignoring case and concatenating the search term added in repository

--- demo/src/main/java/com/example/Project1_SpringMVC/repository/StudentRepository.java
+++ demo/src/main/java/com/example/Project1_SpringMVC/repository/StudentRepository.java
@@ -2,16 +2,16 @@ package com.example.Project1_SpringMVC.repository;
 
 import com.example.Project1_SpringMVC.data.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-    List<Student> findByFirstName(String firstName);
-    List<Student> findByLastName(String lastName);
-    List<Student> findByFirstNameAndLastName(String firstName, String lastName);
+    @Query("SELECT s FROM Student s WHERE LOWER(s.firstName) LIKE LOWER(CONCAT('%', :name, '%')) OR LOWER(s.lastName) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<Student> findByNameContainingIgnoreCase(@Param("name") String name);
     List<Student> findByStudentGroupId(Integer studentGroupId);
-    List<Student> findBySubjectsId(Integer subjectId);
+    List<Student> findBySubjectsId(@Param("subjectId") Integer subjectId);
 }
